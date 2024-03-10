327b968050c1d85beb49375c0ab476876db8aefa
omar aboelnasr <139161001+omaraboelnasr@users.noreply.github.com>
2024-03-10 20:26:41 +0200

    creating the middleware to handle update and delete user by id (#1)
    Co-authored-by: Mostafa_Ali <mostafaqaraman@outlook.com>

--- UsersModule/userModels.js
+++ UsersModule/userModels.js
@@ -1,62 +1,68 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
 
-const userSchema = mongoose.Schema({
-    email: {
-        type: String,
-        required: [true, "email is required"],
-        validate: {
-            validator: function (v) {
-                return /^[a-zA-Z]{3,20}\d*(@)(gmail|yahoo|outlook|hotmail)(.com)$/.test(v);
-            },
-            message: props => `${props.value} is not a valid Email!`
-        },
-        unique: true
-    },
-    username: {
-        type: String,
-        minLength: [5, "username have to be more than 5 char"]
-    },
-    password: {
-        type: String,
-        required: [true, "password is required"],
-        minLength: 3
-    },
-    firstName: {
-        type: String,
-        minLength: 3,
-        maxLength: 15
-    },
-    lastName: {
-        type: String,
-        minLength: 3,
-        maxLength: 15
-    },
-    phone: {
-        type: Number,
-        minLength: 11,
-        maxLength: 11
-    },
-    Gender: {
-        type: String,
-        enum: ["male", "female"]
-    },
-    dob: {
-        type: Date,
-    },
-    owner: {
-        type: Boolean,
-        default: false
-    }
-
-}, { timestamps: true, collection: "user" })
+const userSchema = mongoose.Schema(
+	{
+		email: {
+			type: String,
+			required: [true, "email is required"],
+			validate: {
+				validator: function (v) {
+					return /^[a-zA-Z]{3,20}\d*(@)(gmail|yahoo|outlook|hotmail)(.com)$/.test(
+						v
+					);
+				},
+				message: (props) => `${props.value} is not a valid Email!`,
+			},
+			unique: true,
+		},
+		username: {
+			type: String,
+			//   unique: [true, "username have to be unique"], "removed because if we add another user without a username it will throw an error due to username = null"
+			minLength: [5, "username have to be more than 5 char"],
+		},
+		password: {
+			type: String,
+			required: [true, "password is required"],
+			minLength: 3,
+		},
+		firstName: {
+			type: String,
+			minLength: 3,
+			maxLength: 15,
+		},
+		lastName: {
+			type: String,
+			minLength: 3,
+			maxLength: 15,
+		},
+		phone: {
+			type: Number,
+			minLength: 11,
+			maxLength: 11,
+		},
+		Gender: {
+			type: String,
+			enum: ["male", "female"],
+		},
+		dob: {
+			type: Date,
+		},
+		owner: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	{ timestamps: true, collection: "user" }
+);
 
 userSchema.pre("save", function (next) {
-    var salt = bcrypt.genSaltSync(10);
-    var hash = bcrypt.hashSync(this.password, salt);
-    this.password = hash;
-    next()
-})
-const userModel = mongoose.model("user", userSchema)
+	var salt = bcrypt.genSaltSync(10);
+	var hash = bcrypt.hashSync(this.password, salt);
+	this.password = hash;
+	next();
+});
+const userModel = mongoose.model("user", userSchema);
 
-module.exports = userModel
+module.exports = userModel;
