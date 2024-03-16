1ab1b4a81ac683dbbc67558e230875529847ccf8
Mostafa_Ali <mostafaqaraman@outlook.com>
2024-03-16 19:17:23 +0200

    updated the admin routes to also get all users

--- AdminModule/adminRoutes.js
+++ AdminModule/adminRoutes.js
@@ -6,15 +6,17 @@ const {
 } = require("./adminMiddleware");
 const { register, login } = require("./adminControllers");
 const {
+	getAllUsers,
 	updateUserById,
 	deleteUserById,
 } = require("../UsersModule/userControllers");
 
 router.route("/").post(register);
 router.route("/login").post(login);
-router.use("/users", adminAuthentication);
 router.use("/hotels", adminAuthentication);
 router.use("/bookings", adminAuthentication);
+router.use("/users", adminAuthentication);
+router.route("/users").get(getAllUsers);
 router
 	.route("/users/:id")
 	.patch(adminAuthorization, updateUserById)
