27535901ce37d105a80002be154bdc08e18fa48b
Mostafa_Ali <mostafaqaraman@outlook.com>
2024-03-16 19:46:04 +0200

    fixed issues in roomControlls (wrong param id for rooms), and updating the admin routes to also contain hotels and rooms and handling authentication and authorization on them

--- AdminModule/adminRoutes.js
+++ AdminModule/adminRoutes.js
@@ -4,22 +4,58 @@ const {
 	adminAuthentication,
 	adminAuthorization,
 } = require("./adminMiddleware");
+
+// Admin Routes
 const { register, login } = require("./adminControllers");
+router.route("/").post(register);
+router.route("/login").post(login);
+
+// Users Routes
 const {
 	getAllUsers,
 	updateUserById,
 	deleteUserById,
 } = require("../UsersModule/userControllers");
-
-router.route("/").post(register);
-router.route("/login").post(login);
-router.use("/hotels", adminAuthentication);
-router.use("/bookings", adminAuthentication);
 router.use("/users", adminAuthentication);
 router.route("/users").get(getAllUsers);
 router
 	.route("/users/:id")
 	.patch(adminAuthorization, updateUserById)
 	.delete(adminAuthorization, deleteUserById);
+
+// Hotels Routes
+const {
+	createHotel,
+	getAllHotel,
+	getHotel,
+	updateHotel,
+	deleteHotel,
+} = require("../HotelsModule/hotelControllers");
+router.use("/hotels", adminAuthentication);
+router.route("/hotels").get(getAllHotel).post(createHotel);
+router
+	.route("/hotels/:id")
+	.patch(adminAuthorization, updateHotel)
+	.delete(adminAuthorization, deleteHotel);
+
+// Rooms Routes
+const {
+	createRoom,
+	getAllRooms,
+	getRoom,
+	updateRoom,
+	deleteRoom,
+} = require("../RoomsModule/roomControllers");
+
+router.route("/hotels/rooms/:id").post(createRoom).get(getAllRooms);
+
+router
+	.route("/hotels/room/:id")
+	.get(getRoom)
+	.patch(adminAuthorization, updateRoom)
+	.delete(adminAuthorization, deleteRoom);
+
+// Bookings Routes
+router.use("/bookings", adminAuthentication);
 
 module.exports = router;
