a11ac7b56012365850b8b23f9cedc4cff9ee4e85
omar aboelnasr <139161001+omaraboelnasr@users.noreply.github.com>
2024-03-20 16:02:51 +0200

    add hotels (#8)

--- HotelsModule/hotelRoutes.js
+++ HotelsModule/hotelRoutes.js
@@ -1,9 +1,10 @@
 const express=require('express')
 const router = express.Router()
-const {createHotel,getAllHotel,getHotel,updateHotel,deleteHotel} = require('./hotelControllers')
+const {createHotel,getAllHotel,getHotel,updateHotel,deleteHotel,getHotelByCity} = require('./hotelControllers')
 
 
 router.route("/").post(createHotel).get(getAllHotel)
+router.route("/:city").get(getHotelByCity)
 router.route("/:id").get(getHotel).patch(updateHotel).delete(deleteHotel)
 
 module.exports=router
