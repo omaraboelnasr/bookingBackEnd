c1bc63f29580bc5faac1e386c7d6cce53b3ef606
Mostafa_Ali <mostafaqaraman@outlook.com>
2024-04-16 21:02:55 +0200

    Added AR models to hotels, Modified Login Controller to also send owner state in users

--- HotelsModule/hotelModels.js
+++ HotelsModule/hotelModels.js
@@ -1,80 +1,83 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const HotelSchema = mongoose.Schema({
-    hotelName:{
-        type:String,
-        required:[true,'hotel name is required']
-    },
-    hotelName_ar:{
-        type:String,
-        required:[true,'hotel name is required']
+const HotelSchema = mongoose.Schema(
+	{
+		hotelName: {
+			type: String,
+			required: [true, "hotel name is required"],
+		},
+		hotelName_ar: {
+			type: String,
+			required: [true, "hotel name is required"],
+		},
+		hotelDescription: {
+			type: String,
+			required: [true, "hotel description is required"],
+		},
+		hotelDescription_ar: {
+			type: String,
+			required: [true, "hotel description is required in arabic"],
+		},
+		hotelSubDescription: {
+			type: String,
+			required: [true, "hotel subdescription is required"],
+			minLength: 15,
+			maxLength: 150,
+		},
+		hotelSubDescription_ar: {
+			type: String,
+			required: [true, "hotel subdescription is required in arabic"],
 
-    },
-    hotelDescription:{
-        type:String,
-        required:[true,'hotel description is required']
-    },
-    hotelSubDescription:{
-        type:String,
-        required:[true,'hotel subdescription is required'],
-        minLength: 15,
-		maxLength: 150,
-    },
-    hotelSubDescription_ar:{
-        type:String,
-        required:[true,'hotel subdescription is required'],
+			minLength: 15,
+			maxLength: 150,
+		},
 
-        minLength: 15,
-		maxLength: 150,
-    },
+		hotelMainImage: {
+			type: String,
+			required: [true, "hotel image is required"],
+		},
 
-    hotelMainImage:{
-        type:String,
-        required:[true,'hotel image is required']
-    },
+		hotelCity: {
+			type: String,
+			required: [true, "hotel city is required"],
+			enum: ["cairo", "hurghada", "alexandria", "sharmelsheikh", "dahab"],
+		},
 
-    hotelCity:{
-        type:String,
-        required:[true,'hotel city is required'],
-        enum: ['cairo','hurghada','alexandria','sharmelsheikh','dahab']
-    },
+		hotelCity_ar: {
+			type: String,
+			required: [true, "hotel city is required in arabic"],
 
-    hotelCity_ar:{
-        type:String,
-        required:[true,'hotel city is required'],
+			enum: ["القاهره", "الغردقه", "الاسكندريه", "شرم الشيخ", "دهب"],
+		},
 
-        enum:['القاهره','الغردقه',"الاسكندريه",'شرم الشيخ','دهب']
-    },
+		hotelAddress: {
+			type: String,
+			required: [true, "hotel address is required"],
+		},
+		hotelAddress_ar: {
+			type: String,
+			required: [true, "hotel address is required in arabic"],
+		},
 
-    hotelAddress:{
-        type:String,
-        required:[true,'hotel address is required']
-    },
-    hotelAddress_ar:{
-        type:String,
-        required:[true,'hotel address is required']
+		distanceFromCenter: {
+			type: Number,
+			required: [true, "distance from center is required"],
+		},
+		hotelType: {
+			type: String,
+			required: [true, "type is required"],
+			enum: ["hotel", "apartment", "resort", "villa"],
+		},
+		distanceFromCenter_ar: {
+			type: Number,
+		},
+		hotelRating: {
+			type: String,
+		},
+	},
+	{ collcetion: "hotles" }
+);
 
+const hotelsModel = mongoose.model("hotels", HotelSchema);
 
-    },
-
-    distanceFromCenter:{
-        type:Number,
-        required:[true,'distance from center is required']
-    },
-    hotelType:{
-        type:String,
-        required:[true,'type is required'],
-        enum: ['hotel','apartment','resort','villa']
-    },
-    distanceFromCenter_ar:{
-        type:Number,
-
-    },
-    hotelRating:{
-        type:String,
-    }
-},{collcetion:'hotles'})
-
-const hotelsModel = mongoose.model('hotels',HotelSchema)
-
-module.exports=hotelsModel
+module.exports = hotelsModel;
