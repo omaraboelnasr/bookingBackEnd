889d8fea82c6239313428253d5943b497aebec3a
OmarAboElnasr <omaraboelnasr22@gmail.com>
2024-04-19 09:58:25 +0200

    update

--- HotelsModule/hotelModels.js
+++ HotelsModule/hotelModels.js
@@ -1,51 +1,4 @@
 const mongoose = require("mongoose");
-
-<<<<<<< HEAD
-const HotelSchema = mongoose.Schema(
-	{
-		hotelName: {
-			type: String,
-			required: [true, "hotel name is required"],
-		},
-		hotelDescription: {
-			type: String,
-			required: [true, "hotel description is required"],
-		},
-		hotelSubDescription: {
-			type: String,
-			required: [true, "hotel subdescription is required"],
-			minLength: 15,
-			maxLength: 150,
-		},
-		hotelMainImage: {
-			type: String,
-			required: [true, "hotel image is required"],
-		},
-		hotelCity: {
-			type: String,
-			required: [true, "hotel city is required"],
-			enum: ["cairo", "hurghada", "alexandria", "sharmelsheikh", "dahab"],
-		},
-		hotelAddress: {
-			type: String,
-			required: [true, "hotel address is required"],
-		},
-		distanceFromCenter: {
-			type: Number,
-			required: [true, "distance from center is required"],
-		},
-		hotelType: {
-			type: String,
-			required: [true, "type is required"],
-			enum: ["hotel", "apartment", "resort", "villa"],
-		},
-		hotelRating: {
-			type: String,
-		},
-	},
-	{ collcetion: "hotles" }
-);
-=======
 const HotelSchema = mongoose.Schema({
     hotelName:{
         type:String,
@@ -69,7 +22,6 @@ const HotelSchema = mongoose.Schema({
     hotelSubDescription_ar:{
         type:String,
         required:[true,'hotel subdescription is required'],
-
         minLength: 15,
 		maxLength: 150,
     },
@@ -88,7 +40,6 @@ const HotelSchema = mongoose.Schema({
     hotelCity_ar:{
         type:String,
         required:[true,'hotel city is required'],
-
         enum:['القاهره','الغردقه',"الاسكندريه",'شرم الشيخ','دهب']
     },
 
@@ -99,8 +50,6 @@ const HotelSchema = mongoose.Schema({
     hotelAddress_ar:{
         type:String,
         required:[true,'hotel address is required']
-
-
     },
 
     distanceFromCenter:{
@@ -120,7 +69,6 @@ const HotelSchema = mongoose.Schema({
         type:String,
     }
 },{collcetion:'hotles'})
->>>>>>> main
 
 const hotelsModel = mongoose.model("hotels", HotelSchema);
 
